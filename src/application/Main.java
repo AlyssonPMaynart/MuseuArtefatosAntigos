cec6ecdb7f6936b81a13c8e14971b171279da15b
Igor <igor,profeta@souunit.com.br>
2024-11-14 16:20:10 -0300

    Commit 8 Igor - Classes em geral

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,6 @@ import entities.Visitante;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
